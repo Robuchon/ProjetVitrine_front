c6981bc9b523fd371a26d4e55213210eaaa53b3c
Robuchon Hugues <robuchon.hugues@orange.fr>
2022-10-26 18:09:32 +0200

    setup du services axios auth

---
+++ src/plugins/axios/services/auth.js
@@ -0,0 +1,44 @@
+import Base from "@axios/axios";
+
+class AuthServices {
+  static async login(data) {
+    let body = data;
+    return Base.post(`/auth/login`, body)
+      .then((response) => {
+        console.log("auth : login", response);
+        return response;
+      })
+      .catch((e) => {
+        console.log("error ", e);
+        return e;
+      });
+  }
+
+  static async reset(data) {
+    let body = data;
+    return Base.post(`/auth/reset`, body)
+      .then((response) => {
+        console.log("auth : reset", response);
+        return response;
+      })
+      .catch((e) => {
+        console.log("error ", e);
+        return e;
+      });
+  }
+  
+  static async signin(data) {
+    let body = data;
+    return Base.post(`/auth/signin`, body)
+      .then((response) => {
+        console.log("auth : signin", response);
+        return response;
+      })
+      .catch((e) => {
+        console.log("error ", e);
+        return e;
+      });
+  }
+}
+
+export default AuthServices;

eab9b63ce96391f68b00df9caebb5a8355e2b19d
Robuchon Hugues <robuchon.hugues@orange.fr>
2022-10-26 18:07:53 +0200

    setup du style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'corePattern': "url('@assets/img/bgCore.png')",
+        'corePattern2': "url('@assets/img/bgCore2.png')",
+        'parchemin': "url('@assets/img/parchemin.png')",
+      }
+    },
+    fontFamily: {
+      sans: ["Luminari", ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),

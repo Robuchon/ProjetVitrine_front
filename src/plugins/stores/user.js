115e338d3d1557fe5e6277e6ceab85ba317d08fd
Robuchon Hugues <robuchon.hugues@orange.fr>
2022-10-26 18:09:04 +0200

    utilisation du store pour les paramettre jour nuit

--- src/plugins/stores/user.js
+++ src/plugins/stores/user.js
@@ -6,13 +6,38 @@ const Store = defineStore("user", {
     return {
       // pour l'etat initial vide ou boolean
       isLogin: "",
-      userData: "",
+      userData: {
+        language: "fr",
+        theme: "dark",
+      },
       //pour desactiver certaine fonction du site pendant un loading
       loading: false,
     };
   },
   getters: {},
-  actions: {},
+  actions: {
+    themeTxt() {
+      if (this.userData.theme === "dark") {
+        return "text-orange-200";
+      } else {
+        return "text-orange-900";
+      }
+    },
+    themeBG() {
+      if (this.userData.theme === "dark") {
+        return "bg-gray-800";
+      } else {
+        return "bg-orange-100";
+      }
+    },
+    themeSvg() {
+      if (this.userData.theme === "dark") {
+        return "#FED7AA";
+      } else {
+        return "#7C2D0C";
+      }
+    },
+  },
 });
 
 export default Store;

812146b2ec5727db69d2b336b0c07cfad6abde7d
Robuchon Hugues <robuchon.hugues@orange.fr>
2022-10-30 22:34:08 +0100

    mise en place de toute les page de base

--- src/modules/hall/GestionPage.vue
+++ src/modules/hall/GestionPage.vue
@@ -1,3 +1,5 @@
+<template>
+  <div class="mx-auto flex max-w-7xl flex-col lg:w-[80%]">
     <ProloguePage
       class="min-h-screen bg-corePattern2 py-4"
       v-if="
@@ -6,3 +8,16 @@
         storeUser.userData.prologueEtape < 9
       "
     ></ProloguePage>
+    <GestionContent v-else></GestionContent>
+  </div>
+</template>
+
+<script setup>
+// import components
+import GestionContent from "@modules/gestion/GestionContent.vue";
+import ProloguePage from "@modules/prologue/ProloguePage.vue";
+
+//import store
+import useStoreUser from "@stores/user";
+const storeUser = useStoreUser();
+</script>
